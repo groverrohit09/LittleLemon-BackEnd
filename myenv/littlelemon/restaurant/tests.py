53b926140ed7e939e4d17285148db11d2af0f3e0
Rohit Grover <154079947+groverrohit09@users.noreply.github.com>
2024-03-03 05:52:26 -0800

    Add Unit Test

--- myenv/littlelemon/restaurant/tests.py
+++ myenv/littlelemon/restaurant/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class MenuItemTest(TestCase):
+  def test_get_item(self):
+    item = Menu.objects.create(iD = 1000,title="IceCream", price=80, inventory=100)
+    self.assertEqual(item.title, 'IceCream')
+    self.assertEqual(item.price, 80)
